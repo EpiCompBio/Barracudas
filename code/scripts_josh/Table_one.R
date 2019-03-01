bee96001f5ed24f674eb6ff68c8a0f815464a9af
Joshua Elliott <je108@ic.ac.uk>
2019-03-01 21:52:26 +0000

    table one script

--- code/scripts_josh/Table_one.R
+++ code/scripts_josh/Table_one.R
@@ -5,22 +5,25 @@ mydata = read.csv('data/processed/UKBcompleteFeb19.csv')
 #change gender levels
 mydata$Sex = ifelse(mydata$gender == 0, 'Female','Male')
 
+#define morbidly obese
+mydata$obese = ifelse(mydata$BMI >= 35, 1, 0)
+
 #define outcome cols
-outcomes = c('diabetes','mi','stroke','dvt_asthma_copd_atopy','angina')
+outcomes = c('diabetes','mi','stroke','obese','angina')
 
 outcome_cols = grep(paste0('^',outcomes,'$',collapse = '|'), colnames(mydata))
 
 #col of chronic diseases
 mydata$no_chronic = apply(mydata[,outcome_cols],1,sum)
 
-#subset multi morbid rows
-multi_morbid = mydata[which(mydata$no_chronic>1),]
-
 #binary cols
-binary_cols = which(unlist(sapply(multi_morbid, function(x) length(levels(factor(x)))==2)))
+binary_cols = which(unlist(sapply(mydata, function(x) length(levels(factor(x)))==2)))
 
 #change binary col class
-multi_morbid[,binary_cols] = sapply(multi_morbid[,binary_cols], factor)
+mydata[,binary_cols] = sapply(mydata[,binary_cols], factor)
+
+#subset multi morbid rows
+# multi_morbid = mydata[which(mydata$no_chronic>1),]
 
 vars <- c('birth_year',
           'BMI',
@@ -29,26 +32,26 @@ vars <- c('birth_year',
           'mi',
           'angina',
           'stroke',
-          'dvt_asthma_copd_atopy',
-          'diabetes')
+          'diabetes',
+          'obese')
 catVars <- c('current_smoker',
              'current_etoh',
              'mi',
              'angina',
              'stroke',
-             'dvt_asthma_copd_atopy',
-             'diabetes')
+             'diabetes',
+             'obese')
 strata <- c('Sex',
             'no_chronic'#,
             # 'mi',
             # 'angina',
             # 'stroke',
-            # 'dvt_asthma_copd_atopy',
-            # 'diabetes'
+            # 'diabetes',
+            #'obese'
             )
 table1 <- CreateTableOne(vars = vars,
                          strata = strata,
-                         factorVars = catVars, data = multi_morbid )
+                         factorVars = catVars, data = mydata)
 Table1Matrix <- print(table1, quote=FALSE, noSpaces = TRUE, contDigits=1)
 write.csv(Table1Matrix,'Barracudas/descriptive_analysis_results/tableone.csv')
 summary(table1)
