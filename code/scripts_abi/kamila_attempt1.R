cbacac8d503f10b67bf6282db4f3597a54845742
aeast2818 <eastabigail@yahoo.co.uk>
2019-03-18 15:44:55 +0000

    Added interpretation/evaluation plots

--- code/scripts_abi/kamila_attempt1.R
+++ code/scripts_abi/kamila_attempt1.R
@@ -25,6 +25,7 @@ mydata=read.csv("Data/UKBcompleteFeb19.csv")
 
 source("Git_Repo/code/utility_functions/FAMD_plots_utility.R")
 source("Git_Repo/code/utility_functions/colors_themes_utility.R")
+source("Git_Repo/code/utility_functions/clustering_utility.R")
 
 
 ################################################################################
@@ -55,6 +56,9 @@ mydata=mydata %>% select(eid,mi,angina,stroke,htn,obese,no_chronic, everything()
 
 #subset multi morbid rows
 multi_morbid = mydata[which(mydata$no_chronic>1),]
+
+#remove asthma variable 
+multi_morbid$dvt_asthma_copd_atopy <- NULL
 
 #Set eid as rownames
 rownames(multi_morbid) <- multi_morbid[,1]
@@ -90,6 +94,7 @@ saveRDS(multi_morbid, file = "Data/mm_scaled.rds")
 ################################################################################
 
 multi_morbid <- readRDS("Data/mm_scaled.rds")
+multi_morbid <- multi_morbid[ ,-c(1:6)]
 
 FAMD_kamila_cluster=FAMD(multi_morbid, ncp = ncol(multi_morbid), graph = FALSE)
 
@@ -97,7 +102,7 @@ FAMD_kamila_cluster=FAMD(multi_morbid, ncp = ncol(multi_morbid), graph = FALSE)
 # Choosing number of clusters for Kamila algorithm
 ################################################################################
 
-kamRes <- kamila(multi_morbid[,7:59], multi_morbid[,60:77], numClust = 2:8, numInit = 10,
+kamRes <- kamila(multi_morbid[,1:53], multi_morbid[,54:71], numClust = 2:8, numInit = 10,
                  calcNumClust = "ps",numPredStrCvRun = 10, predStrThresh = 0.5)
 
 kamila_cluster_choice <- plot(2:8, kamRes$nClust$psValues,
@@ -119,29 +124,29 @@ dev.off()
 ################################################################################
 
 # k=3
-kamila_cluster_3 <- kamila(multi_morbid[,7:59], multi_morbid[,60:77], numClust = 3, numInit = 10)
+kamila_cluster_3 <- kamila(multi_morbid[,1:53], multi_morbid[,54:71], numClust = 3, numInit = 10)
 
 kamila_cluster_plot_3=make_FAMD_ind_plot_classes(FAMD_kamila_cluster,
-                                                          classes=as.factor(kamila_cluster$finalMemb),dims=c(1,2),
+                                                          classes=as.factor(kamila_cluster_3$finalMemb),dims=c(1,2),
                                                           custom_theme=theme_jh,color_scale=distinct_scale)
 
-table(kamila_cluster$finalMemb)
+table(kamila_cluster_3$finalMemb)
 
 svg("kamila_cluster_plot_3.svg")
 kamila_cluster_plot_3
 dev.off()
 
 # k=2
-kamila_cluster_2 <- kamila(multi_morbid[,7:59], multi_morbid[,60:77], numClust = 2, numInit = 10)
+kamila_cluster_2 <- kamila(multi_morbid[,1:53], multi_morbid[,54:71], numClust = 2, numInit = 10)
 
 kamila_cluster_plot_2=make_FAMD_ind_plot_classes(FAMD_kamila_cluster,
-                                               classes=as.factor(kamila_cluster$finalMemb),dims=c(1,2),
+                                               classes=as.factor(kamila_cluster_2$finalMemb),dims=c(1,2),
                                                custom_theme=theme_jh,color_scale=distinct_scale)
 
-table(kamila_cluster$finalMemb)
+table(kamila_cluster_2$finalMemb)
 
-svg("kamila_cluster_plot.svg")
-kamila_cluster_plot
+svg("Git_Repo/code/results_abi/kamila_cluster_plot_2.svg")
+kamila_cluster_plot_2
 dev.off()
 
 ################################################
@@ -155,8 +160,12 @@ color_scale=NULL
 custom_theme=theme_jh
 title=NULL
 
+cat_variables=colnames(multi_morbid)[sapply(multi_morbid,class) == "factor"]
+cont_variables=colnames(multi_morbid)[sapply(multi_morbid,class) != "factor"]
 
-mean_by_cluster_continuous_kamila=function(data,classes,color_scale=NULL,custom_theme=NULL,title=NULL) {
+#####cont_variables=cont_variables[2:length(cont_variables)]#####
+
+mean_by_cluster_continuous=function(data,classes,color_scale=NULL,custom_theme=NULL,title=NULL) {
   
   data_and_classes = data.frame(data, classes)
   means_clusters=aggregate(data_and_classes[,-c(ncol(data_and_classes))],
@@ -184,20 +193,207 @@ mean_by_cluster_continuous_kamila=function(data,classes,color_scale=NULL,custom_
   return(mean_clusters_plot)
 }
 
-cat_variables=colnames(multi_morbid)[sapply(multi_morbid,class) == "factor"]
-cont_variables=colnames(multi_morbid)[sapply(multi_morbid,class) != "factor"]
-cont_variables=cont_variables[2:length(cont_variables)]
 
-
-kmeans_KAMILA_mean_by_cluster_continuous_plot=mean_by_cluster_continuous_kamila(data=multi_morbid[,cont_variables],
+KAMILA_mean_by_cluster_continuous_plot=mean_by_cluster_continuous(data=multi_morbid[,cont_variables],
                                                                        classes=as.factor(kamila_cluster_2$finalMemb),
                                                                        color_scale=NULL,custom_theme=theme_jh,title=NULL)
 
 
 svg(filename="Git_Repo/code/results_abi/kamila_multi_morbid_mean_by_cluster_continuous_plot.svg",width=10,height=10)
-print(kmeans_KAMILA_mean_by_cluster_continuous_plot)
+print(KAMILA_mean_by_cluster_continuous_plot)
 dev.off()
 
 #########################################################
 # Distribution of continuous/categorical vars by cluster
 #########################################################
+cat_distribution_by_cluster=function(data,classes,layout=NULL,color_scale=NULL,custom_theme=NULL,title=NULL) {
+  
+  if (is.null(layout)) {
+    layout=c(ceiling(sqrt(ncol(data))),ceiling(sqrt(ncol(data))))
+  }
+  
+  plot_list=list()
+  
+  for (k in 1:ncol(data)) {
+    
+    
+    tmp_plot_data=as.data.frame(prop.table(table(data.frame(data[,k,drop=FALSE], classes)),margin =2))
+    
+    tmp_plot_data$n_ind=as.data.frame(table(data.frame(data[,k,drop=FALSE], classes)))[,3]
+    tmp_plot_data=tmp_plot_data %>%
+      arrange(.[[2]], desc(.[[1]]))
+    tmp_plot_data$position=as.data.frame((tmp_plot_data %>% group_by(classes) %>% dplyr::mutate(cumsum(Freq))))[,5]
+    tmp_plot_data$position=tmp_plot_data$position - tmp_plot_data$Freq/2
+    
+    tmp_plot=ggplot(data=tmp_plot_data) +
+      geom_bar(aes_string(x="classes",y="Freq",fill=paste0(colnames(data)[k])),stat="identity",position="stack") +
+      xlab("") + ylab("") + custom_theme + geom_text(aes(x=classes,y=position,label=n_ind),check_overlap = TRUE,
+                                                     size = 4,color="black")
+    
+    
+    
+    if(!is.null(color_scale)) {
+      tmp_plot = tmp_plot + scale_fill_manual(values=color_scale)
+    }
+    
+    
+    plot_list[[k]]=tmp_plot
+  }
+  
+  
+  final_plot=grid.arrange(gTree(children = gList(rectGrob(gp=gpar(fill="white",col="white", lwd=0)),
+                                                 arrangeGrob(arrangeGrob(grobs=plot_list, ncol=layout[1], nrow=layout[2]),
+                                                             top = textGrob(paste0(title),
+                                                                            gp=gpar(font=2,fontsize=15)),
+                                                             left = textGrob(paste0("Proportion"),
+                                                                             gp=gpar(font=2,fontsize=12),rot = 90),
+                                                             bottom = textGrob(paste0("Classes"),
+                                                                               gp=gpar(font=2,fontsize=12),rot = 0)
+                                                 ))))
+  
+  return(final_plot)
+  
+}
+
+cat_variables_split=splitIndices(nx=length(cat_variables), ncl=ceiling(length(cat_variables) / 9))
+
+for (k in 1:length(cat_variables_split)) {
+  
+  
+  kamila_cat_distribution_by_cluster=cat_distribution_by_cluster(data=multi_morbid[,cat_variables[cat_variables_split[[k]]]],
+                                                                      classes=as.factor(kamila_cluster_2$finalMemb),layout=c(3,3),
+                                                                      color_scale=NULL,custom_theme=theme_jh,
+                                                                      title=paste0("Distributions of categorical variables by classes (",
+                                                                                   k,"/",length(cat_variables_split),")"))
+  
+  
+  svg(filename=paste0("Git_Repo/code/results_abi/kamila_cat_distributions_",k,"_",length(cat_variables_split),".svg"),
+      width=10,height=10)
+  grid.draw(kamila_cat_distribution_by_cluster)
+  dev.off()
+  
+}
+
+#########################################################
+# Distribution of continuous vars by cluster
+#########################################################  
+
+cont_distribution_by_cluster=function(data,classes,layout=NULL,color_scale=NULL,custom_theme=NULL,title=NULL) {
+  
+  if (is.null(layout)) {
+    layout=c(ceiling(sqrt(ncol(data))),ceiling(sqrt(ncol(data))))
+  }
+  
+  plot_list=list()
+  
+  for (k in 1:ncol(data)) {
+    
+    
+    density_list_raw=by(as.numeric(data[,k]),INDICES=as.numeric(as.character(classes)),density,simplify = TRUE)
+    
+    classes_vector=NULL
+    classes_list=lapply(density_list_raw,function(x) {length(x[[1]])})
+    
+    for (i in 1:length(density_list_raw)) {
+      classes_vector=c(classes_vector,rep(names(classes_list[i]), classes_list[i]))
+    }
+    
+    tmp_plot_data=data.frame(x=unlist(lapply(density_list_raw,function(x) {x[[1]]})),
+                             y=unlist(lapply(density_list_raw,function(x) {x[[2]]})),
+                             classes=as.factor(classes_vector))
+    
+    tmp_plot=ggplot(data=tmp_plot_data) +
+      geom_line(aes(x=x,y=y,color=classes),alpha=1) +
+      xlab("") + ylab("") + custom_theme + ggtitle(colnames(data)[k]) + custom_theme
+    if(!is.null(color_scale)) {
+      tmp_plot = tmp_plot + scale_color_manual(values=distinct_scale)
+    }
+    
+    plot_list[[k]]=tmp_plot
+  }
+  
+  final_plot=grid.arrange(gTree(children = gList(rectGrob(gp=gpar(fill="white",col="white", lwd=0)),
+                                                 arrangeGrob(grobs=plot_list, ncol=layout[1], nrow=layout[2])
+  )))
+  
+  return(final_plot)
+  
+}
+  
+cont_variables_split=splitIndices(nx=length(cont_variables), ncl=ceiling(length(cont_variables) / 9))
+  
+  
+  for (k in 1:length(cont_variables_split)) {
+    
+    kamila_cont_distribution_by_cluster=cont_distribution_by_cluster(data=multi_morbid[,cont_variables[cont_variables_split[[k]]]],
+                                                                          classes=as.factor(kamila_cluster_2$finalMemb),layout=c(3,3),
+                                                                          color_scale=NULL,custom_theme=theme_jh,
+                                                                          title=paste0("Distributions of continuous variables by classes (",
+                                                                                       k,"/",length(cont_variables_split),")"))
+    
+    svg(filename=paste0("Git_Repo/code/results_abi/kamila_cont_distributions_",k,"_",length(cont_variables_split),".svg"),
+        width=10,height=10)
+    grid.draw(kamila_cont_distribution_by_cluster)
+    dev.off()
+    }
+
+#########################################################
+# Heatmaps of variables
+#########################################################
+
+library(corrplot)
+svg("Git_Repo/code/results_abi/KAMILA/corrplot_cont_vars.svg")
+corrplot(cor(multi_morbid[,1:53]))  
+dev.off()
+
+library(pheatmap)
+svg("Git_Repo/code/results_abi/KAMILA/heatmap_cont_vars.svg")
+pheatmap(cor(multi_morbid[,1:53]))
+dev.off()
+
+########################################################################################################
+# Plot a silhouette plot (cluster integrity)
+#### INPUTS
+# data : data used for clustering
+# classes : clusters
+# daisy_metric : metric for the daisy function to use
+# custom_theme : custom ggplot theme to be applied
+# color_scale : color scale for the plot
+# title : title for the plot
+#### OUTPUT
+# ggplot object of the silhouette plot
+########################################################################################################
+
+silhouette_plot_ggplot2=function(data,classes,daisy_metric="euclidean",custom_theme=NULL,color_scale=NULL,title=NULL) {
+  
+  
+  
+  dissimilarity_matrix <-  daisy(data,metric=daisy_metric)
+  silhouette_object <- silhouette(as.numeric(as.character(classes)), dissimilarity_matrix)
+  attr(silhouette_object,"class")=NULL
+  silhouette_data=as.data.frame(as.matrix(silhouette_object))
+  silhouette_data=silhouette_data[with(silhouette_data,order(silhouette_data[,1],silhouette_data[,3],decreasing=TRUE)),]
+  silhouette_data$id=1:nrow(silhouette_data)
+  silhouette_data[,"cluster"]=as.factor(silhouette_data[,'cluster'])
+  
+  silhouette_plot = ggplot(silhouette_data) + geom_bar(aes(x=id,y=sil_width,fill=cluster),stat = "identity",position = "dodge")+
+    xlab(" ") + ylab("Silhouette width") +  custom_theme
+  if (!is.null(color_scale)) {
+    scale_fill_manual(name="Clusters", labels=levels(silhouette_data$cluster),
+                      values=color_scale)
+  }
+  
+  return(silhouette_plot)
+}
+
+## Use unscaled data ##
+
+mm_unscaled <- readRDS("Data/mm_unscaled.rds")
+mm_unscaled <- mm_unscaled[ , -c(1:6)]
+
+sil_plot = silhouette_plot_ggplot2(data = mm_unscaled, classes = kamila_cluster_2$finalMemb)
+svg("Git_Repo/code/results_abi/KAMILA/silhoutte_kamila.svg")
+sil_plot
+dev.off()
+
+
