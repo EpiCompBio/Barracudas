e9b576a6c80a95f696d08656a9e892ebfb3277cd
aeast2818 <eastabigail@yahoo.co.uk>
2019-04-08 22:23:26 +0100

    Scripts for new dataset

--- code/scripts_abi/KAMILA_V2/preprocess_ordinal_for_kamila.R
+++ code/scripts_abi/KAMILA_V2/preprocess_ordinal_for_kamila.R
@@ -1,4 +1,7 @@
 setwd("/Users/abieast/Documents/Imperial/Translational_Data_Science/Project")
+
+##### Scaled Data #####
+
 multi_morbid <- readRDS("Data/KAMILA_V2/multi_morbid_ordinal_keep.rds")
 
 # Remove outcome columns
@@ -10,3 +13,17 @@ multi_morbid<- multi_morbid[ , c(1, 2, 4, 6, 7, 8, 9, 10,11,12, 16,17,18,20,21,2
                                                     57,58,59,60,61,62,63,65)]
 
 saveRDS(multi_morbid,"Data/KAMILA_V2/kamila_multi_morbid_ordinal_keep.rds")
+
+##### Unscaled Data #####
+
+multi_morbid_unscaled <- readRDS("Data/KAMILA_V2/unscaled_multi_morbid_ordinal_keep.rds")
+
+# Remove outcome columns
+multi_morbid_unscaled<- multi_morbid_unscaled[ ,-c(1:9)]
+
+# Reorder columns
+multi_morbid_unscaled<- multi_morbid_unscaled[ , c(1, 2, 4, 6, 7, 8, 9, 10,11,12, 16,17,18,20,21,22,23,24,25,34,35,37,38,39,42,43,44,45,
+                                 46,47,64,3,5,13,14,15,19,26,27,28,29,30,31,32,33,36,40,41,48,49,50, 51,52,53,54,55,56,
+                                 57,58,59,60,61,62,63,65)]
+
+saveRDS(multi_morbid_unscaled,"Data/KAMILA_V2/unscaled_kamila_multi_morbid_ordinal_keep.rds")
